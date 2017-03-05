0f07359c3dc8d918807e7befe1d0f4f04e628094
xierh <xieruohong@gmail.com>
2017-03-05 21:47:28 +0800

    Add signal handler function

--- chp5/tcpechoserv.c
+++ chp5/tcpechoserv.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <limits.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -7,6 +8,19 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+/* handle SIG_CHILD */
+void sig_child(int signo) 
+{
+	pid_t pid;
+	int stat;
+	
+	pid = wait(&stat);
+	printf("Child %d terminated\n", pid);
+	
+	return;
+}
+
+/* Read string from socket and write back */
 void str_echo (int sockfd)
 {
 	char buf[LINE_MAX];
@@ -33,6 +47,21 @@ int main(int argc, char *argv[])
 	int sa_len;
 	char buf[1024];
 	pid_t c_pid;
+	struct sigaction act, oldact;
+
+	/* Set up signal handler function */
+	act.sa_handler = sig_child;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+
+	/* If we don't set this flag, syscall 'accept' will not
+	 * be restarted and return an EINTR */
+	act.sa_flags |= SA_RESTART;
+
+	if (sigaction(SIGCHLD, &act, &oldact) < 0) {
+		perror(strerror(errno));
+		exit(1);
+	}
 
 	/* Create socket */
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -56,7 +85,16 @@ int main(int argc, char *argv[])
 	}
 
 	while (1) {
-		connfd = accept(listenfd, (struct sockaddr*)&cli_sa, &sa_len);
+		if ( (connfd = accept(listenfd, (struct sockaddr*)&cli_sa, &sa_len)) < 0) {
+			// We are not sure if SA_RESTART is work as expected in all platform
+			if (errno != EINTR) {	
+				perror(strerror(errno));
+				exit(1);
+			} else {
+				continue;
+			}
+		}
+
 		c_pid = fork();
 		if (c_pid == 0) {	/* child */
 			str_echo(connfd);
